a024288984430248570eef3943731bb1648b35fc
Fitzonreee <kevin.fitzhenry@createthenext.com>
2018-02-08 22:44:12 -0800

    Added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,8 @@
-console.log('This is the JS file');
+const menuIcon = document.querySelector('.title-bar > .menuIcon')
+const menuBar = menuIcon.querySelector('.bar')
+const topBar = document.querySelector('.top-bar')
+
+menuIcon.addEventListener('click', function() {
+  topBar.classList.toggle('open')
+  menuBar.classList.toggle('animate')
+})
